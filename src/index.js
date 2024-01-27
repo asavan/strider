1540ee74a82de42e4c4d4b0faeeaf672434b560c
a.savanovich <asavanster@gmail.com>
2024-01-27 12:23:10 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// eslint-disable-next-line no-undef
+if (__USE_SERVICE_WORKERS__) {
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register("./sw.js", {scope: "./"});
+        install(window, document);
+    }
+}
